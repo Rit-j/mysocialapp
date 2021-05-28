42a1445826eaf407691a437f426c6cef38c9759c
Rit-j <84802049+Rit-j@users.noreply.github.com>
2021-05-28 13:28:32 +0530

    Update commentviewholder.java

--- app/src/main/java/com/example/mysocialapp/commentviewholder.java
+++ app/src/main/java/com/example/mysocialapp/commentviewholder.java
@@ -17,9 +17,9 @@ public class commentviewholder extends RecyclerView.ViewHolder
     public commentviewholder(@NonNull View itemView) {
         super(itemView);
 
-        cuimage=itemView.findViewById(R.id.cuimage);
-        cuname=itemView.findViewById(R.id.cuname);
-        cumessage=itemView.findViewById(R.id.cumessage);
-        cudt=itemView.findViewById(R.id.cudt);
+        cuimage = itemView.findViewById(R.id.cuimage);
+        cuname = itemView.findViewById(R.id.cuname);
+        cumessage = itemView.findViewById(R.id.cumessage);
+        cudt = itemView.findViewById(R.id.cudt);
     }
 }
